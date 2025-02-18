4b7c67af1e677d0be3ae570543c1b235b4eb505d
Mikhail Kapytok <mishakopytok@gmail.com>
2025-02-19 00:12:51 +0300

    registration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
